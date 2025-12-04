0e3062fad7b989faf04f05981f481230a36cb075
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-12-05 04:36:55 +0700

    feat: Add functional burger menu

--- dist/components/navbar.js
+++ dist/components/navbar.js
@@ -4,33 +4,35 @@ class AppNavbar extends BaseComponent {
         return /*html*/ `
 			<link rel="stylesheet" href="src/styles/Navbar.css" />
       <nav>
-				<ul class="navbar-menu">
-					<li><a href="#home">> HOME</a></li>
-					<li><a href="#about">> ABOUT</a></li>
-					<li><a href="#project">> PROJECT</a></li>
-					<li><a href="#contact">> CONTACT</a></li>
-				</ul>
-				<div class="navbar-container">
-					<div class="navbar-logo"><span>zal.</span></div>
-					<div class="navbar-bar"></div>
-					<button class="navbar-burger" for="menu-toggler">▧</button>
+				<div class="navbar">
+					<ul class="navbar-menu">
+						<li><a href="#home">> HOME</a></li>
+						<li><a href="#about">> ABOUT</a></li>
+						<li><a href="#project">> PROJECT</a></li>
+						<li><a href="#contact">> CONTACT</a></li>
+					</ul>
+					<div class="navbar-container">
+						<div class="navbar-logo"><span>zal.</span></div>
+						<div class="navbar-bar"></div>
+						<button class="navbar-burger" for="menu-toggler">▧</button>
+					</div>
 				</div>
-			</nav>`;
+				<div class="scrollbar"></div>
+			</nav>
+			`;
     }
     addEventListeners() {
-        const toggle = this.shadowRoot.getElementById('menu-toggler');
+        const burger = this.shadowRoot.querySelector('.navbar-burger');
         const menu = this.shadowRoot.querySelector('.navbar-menu');
-        if (toggle && menu) {
-            toggle.addEventListener('change', (e) => {
-                const target = e.target;
-                if (target.checked) {
-                    menu.style.display = 'block';
-                }
-                else {
-                    menu.style.display = '';
-                }
-            });
-        }
+        if (!burger || !menu)
+            return;
+        burger.addEventListener('click', () => {
+            const isVisible = menu.style.display === 'block';
+            if (isVisible)
+                menu.style.display = 'none';
+            else
+                menu.style.display = 'block';
+        });
     }
 }
 customElements.define('app-navbar', AppNavbar);
--- src/components/navbar.ts
+++ src/components/navbar.ts
@@ -6,35 +6,37 @@ class AppNavbar extends BaseComponent {
 		return /*html*/`
 			<link rel="stylesheet" href="src/styles/Navbar.css" />
       <nav>
-				<ul class="navbar-menu">
-					<li><a href="#home">> HOME</a></li>
-					<li><a href="#about">> ABOUT</a></li>
-					<li><a href="#project">> PROJECT</a></li>
-					<li><a href="#contact">> CONTACT</a></li>
-				</ul>
-				<div class="navbar-container">
-					<div class="navbar-logo"><span>zal.</span></div>
-					<div class="navbar-bar"></div>
-					<button class="navbar-burger" for="menu-toggler">▧</button>
+				<div class="navbar">
+					<ul class="navbar-menu">
+						<li><a href="#home">> HOME</a></li>
+						<li><a href="#about">> ABOUT</a></li>
+						<li><a href="#project">> PROJECT</a></li>
+						<li><a href="#contact">> CONTACT</a></li>
+					</ul>
+					<div class="navbar-container">
+						<div class="navbar-logo"><span>zal.</span></div>
+						<div class="navbar-bar"></div>
+						<button class="navbar-burger" for="menu-toggler">▧</button>
+					</div>
 				</div>
-			</nav>`;
+				<div class="scrollbar"></div>
+			</nav>
+			`;
 	}
 
 
 	addEventListeners(): void {
-		const toggle = this.shadowRoot.getElementById('menu-toggler') as HTMLInputElement | null;
-		const menu = this.shadowRoot.querySelector('.navbar-menu') as HTMLElement | null;
+		const burger = this.shadowRoot.querySelector('.navbar-burger');
+		const menu = this.shadowRoot.querySelector('.navbar-menu') as HTMLElement;
 
-		if (toggle && menu) {
-			toggle.addEventListener('change', (e: Event) => {
-				const target = e.target as HTMLInputElement;
-				if (target.checked) {
-					menu.style.display = 'block';
-				} else {
-					menu.style.display = '';
-				}
-			});
-		}
+		if (!burger || !menu) return;
+
+		burger.addEventListener('click', () => {
+			const isVisible = menu.style.display === 'block';
+
+			if (isVisible) menu.style.display = 'none';
+			else menu.style.display = 'block';
+		});
 	}
 }
 
