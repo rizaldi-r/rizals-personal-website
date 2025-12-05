e145ffa0606d195cdd2079cf96779334677c511f
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-12-05 13:17:00 +0700

    style: Apply styling to home section

--- dist/components/Home.js
+++ dist/components/Home.js
@@ -2,28 +2,33 @@ import { BaseComponent } from './BaseComponent.js';
 class AppHome extends BaseComponent {
     template() {
         return /*html*/ `
-			<link rel="stylesheet" href="src/styles/Home.css" />
-			<section id="home" class="home">
-				<div class="home-title">
-					<h1>Rizaldi R.</h1>
-				</div>
+		<link rel="stylesheet" href="src/styles/Home.css" />
+		<link rel="stylesheet" href="src/styles/utility.css" />
+		<section class="home">
+			<div class="home-title">
+				<h1>Rizaldi R.</h1>
+			</div>
 
-				<h2 class="home-tagline">Aspiring Fullstack Developer</h2>
+			<div class="home-container">
+				<h2 class="home-tagline">Fullstack Dev/ </br>
+				Game Dev</h2>
 				<div class="home-loading-container">
 					<span class="home-loading-bit">>></span>
 					<span class="home-loading-bit">>></span>
 					<span class="home-loading-bit">>></span>
-					<p class="home-loading">[~~~~~~~~~~~~~~]</p>
+					<p class="home-loading-bar">[~~~~~~~~~~~~~~]</p>
 				</div>
 
 				<div class="home-text">
 					<p>
-						> I create stuff through code.
+						> I create stuff through </br> 
+						code.
 					</p>
 					<p>> <span class="cursor-anim">██</span></p>
 				</div>
-			</section>
-			`;
+			</div>
+		</section>
+		`;
     }
     addEventListeners() { }
 }
--- src/components/Home.ts
+++ src/components/Home.ts
@@ -4,31 +4,36 @@ class AppHome extends BaseComponent {
 
 	template(): string {
 		return /*html*/`
-			<link rel="stylesheet" href="src/styles/Home.css" />
-			<section id="home" class="home">
-				<div class="home-title">
-					<h1>Rizaldi R.</h1>
-				</div>
+		<link rel="stylesheet" href="src/styles/Home.css" />
+		<link rel="stylesheet" href="src/styles/utility.css" />
+		<section class="home">
+			<div class="home-title">
+				<h1>Rizaldi R.</h1>
+			</div>
 
-				<h2 class="home-tagline">Aspiring Fullstack Developer</h2>
+			<div class="home-container">
+				<h2 class="home-tagline">Fullstack Dev/ </br>
+				Game Dev</h2>
 				<div class="home-loading-container">
 					<span class="home-loading-bit">>></span>
 					<span class="home-loading-bit">>></span>
 					<span class="home-loading-bit">>></span>
-					<p class="home-loading">[~~~~~~~~~~~~~~]</p>
+					<p class="home-loading-bar">[~~~~~~~~~~~~~~]</p>
 				</div>
 
 				<div class="home-text">
 					<p>
-						> I create stuff through code.
+						> I create stuff through </br> 
+						code.
 					</p>
 					<p>> <span class="cursor-anim">██</span></p>
 				</div>
-			</section>
-			`;
+			</div>
+		</section>
+		`;
 	}
 
-	addEventListeners(): void {}
+	addEventListeners(): void { }
 }
 
 customElements.define('app-home', AppHome);
