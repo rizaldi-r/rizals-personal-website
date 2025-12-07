69ec94a6ddb14f8c469fac2fb970e3ee8ed928b9
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-12-07 22:21:42 +0700

    style: Style contact section for desktop

--- dist/components/Contact.js
+++ dist/components/Contact.js
@@ -3,10 +3,10 @@ class AppContact extends BaseComponent {
     constructor() {
         super(...arguments);
         this.socials = [
-            { title: "Email", link: "mailto:m.rizaldir@outlook.com" },
+            { title: "Email", link: "mailto:m.rizaldir)outlook.com" },
             { title: "Github", link: "https://github.com/rizaldi-r" },
-            { title: "Linkdn", link: "https://www.linkedin.com/in/rizaldi-ramli/" },
-            { title: "X (Twitter)", link: "https://www.pinterest.com/" }
+            { title: "Linkedin", link: "https://www.linkedin.com/in/rizaldi-ramli/" },
+            { title: "X (Twitter)", link: "https://x.com/rizaldirl" }
             // { title: "Instagram", link: "https://www.instagram.com/" },
         ];
     }
@@ -16,18 +16,26 @@ class AppContact extends BaseComponent {
 		<link rel="stylesheet" href="src/styles/_common/utility.css" />
 		
 		<section id="contact" class="contact section">
-			<h2 class="contact-title sub-heading">
-				Catch my Socials <span class="mobile-break">*</span>
-			</h2>
-			
-			<div class="contact-socials">
-				${this.socials.map(social => `
-					<span class="hover-right">
-						<a target="_blank" href="${social.link}">
-							> ${social.title}
-						</a>
-					</span>
-				`).join('')}
+			<div class="contact-container">
+				<h2 class="contact-title sub-heading">
+					Catch my Socials <span class="mobile-break">*</span>
+				</h2>
+				
+				<div class="contact-socials">
+					${this.socials.map(social => /*html*/ `
+						<span class="hover-right">
+							<a target="_blank" href="${social.link}">
+								> ${social.title}
+							</a></br>
+						</span>
+					`).join('')}
+				</div>
+			</div>
+
+			<div class="contact-devider">
+				<span class="loop-dot">
+					):):):):):):):):):):):):):):):):):):):):):):):):):):):):):
+				</span>
 			</div>
 
 			<footer class="footer">
